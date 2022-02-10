76da52d10b797777835b977002bf2c242682e773
AlekseyMohylnyi <alloexey@gmail.com>
2022-02-10 16:29:42 +0200

    add block

---
+++ src/components/Clock.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="clock">
+        <div class="clock__title">Time Laps</div>
+        <div class="clock__face">
+            <h4 class="clock__day">Sunday</h4>
+            <h3 class="clock__date">Sep 24, 2020</h3>
+            <h2 class="clock__time">08 : 30</h2>
+            <h4 class="clock__alarm">
+                <div class="clock__alarm-time">9:00</div>
+                <div class="clock__alarm-icon">
+                    <img src="../assets/images/icons/alarm-icon.svg" alt="alarm">
+                </div>
+            </h4>
+        </div>
+    </div>
+</template>
+
+<style>
+.clock__title {
+  font-family: 'Poppins-SemiBold';
+  font-size: 1rem;
+  color: var(--green);
+  text-align: center;  
+}
+
+.clock__face {
+    border: 5px solid var(--dark);
+    margin: 3.75rem;
+    padding: 5rem;
+    padding-top: 2rem;
+    text-align: center;
+    border-radius: 50%;
+}
+
+.clock__day {
+    font-family: 'Poppins-Medium';
+    font-size: 1rem;
+}
+</style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <div class="top">
+      <img class="top__icon" src="../assets/images/icons/video-camera 1.svg" alt="camera">      
+    </div>   
+    <Clock /> 
+  </div>
+</template>
+
+<script>
+import Clock from '../components/Clock.vue'
+export default {
+  name: 'Wrapper',
+  components: {
+    Clock
+  }
+}
+</script>
+
+
+<style scoped>
+.wrapper {
+  margin: 100px auto;
+  background-color: var(--background);  
+  border-radius: 20px;
+  width: 414px;
+  height: 896px;
+  position: relative;
+  overflow: hidden;
+  padding-top: 180px;
+}
+
+.top {
+  position: absolute;
+  width: 414px;
+  height: 414px;
+  left: 0;
+  top: -266px;
+  background: #2D2D33;  
+  border: 5px dashed rgba(69, 71, 86, 0.65);  
+  box-shadow: inset 0px -5px #2D2D33; 
+  border-radius: 50%;   
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  
+  align-items: center; 
+}
+
+.top__icon {
+  width: 61px;
+  margin-top: 14.5rem;
+}  
+
+</style>
